1ff6f98652d62a6d68a65bcdf69247011f7ee99f
Daniel Murphy <tugs.murphy@yahoo.com>
2018-12-28 12:47:45 -0500

    added to drafting.js

--- public/js/drafting.js
+++ public/js/drafting.js
@@ -9,9 +9,21 @@ getPlayers();
 // create function that places all players into rows/comlumns with the correct info
 function createPlayerRow(playerData) {
     var newTr = $("<tr>");
-    newTr.data("pprPLayer", playerData);
-    newTr.append("<td>" + playerData.player_name + "</td>");
-    // newTr.append("<td> " + playerData.Posts.length + "</td>");
+    newTr.data("standardPlayer", playerData);
+    newTr.append("<td> " + playerData.rank + "</td>");
+    newTr.append("<td> " + playerData.position_rank + "</td>");
+    newTr.append("<td> " + playerData.player_name + "</td>");
+    newTr.append("<td> " + playerData.nfl_team + "</td>");
+    newTr.append("<td> " + playerData.draft_avg + "</td>");
+    newTr.append("<td> " + playerData.bye_week + "</td>");
+    var draftButton = $("<a>");
+    draftButton.addClass("btn btn-success btn-sm draft-player-btn");
+    draftButton.text("D");
+    var draftedButton = $("<a>");
+    draftedButton.addClass("btn btn-danger btn-sm remove-player-btn");
+    draftButton.text("AD");
+    newTr.append(draftButton, draftedButton);
+    
     // newTr.append("<td><a href='/blog?author_id=" + authorData.id + "'>Go to Posts</a></td>");
     // newTr.append("<td><a href='/cms?author_id=" + authorData.id + "'>Create a Post</a></td>");
     // newTr.append("<td><a style='cursor:pointer;color:red' class='delete-author'>Delete Author</a></td>");
@@ -20,22 +32,22 @@ function createPlayerRow(playerData) {
 
 // create function the gets the players
 function getPlayers() {
-    $.get("/api/pprPlayers", function(data) {
+    $.get("/api/standardPlayers", function(data) {
       var rowsToAdd = [];
       for (var i = 0; i < data.length; i++) {
         rowsToAdd.push(createPlayerRow(data[i]));
       }
       renderPlayerList(rowsToAdd);
-      nameInput.val("");
+      
     });
   }
 
-  // A function for rendering the list of authors to the page
+  // A function for rendering the list of players to the page
   function renderPlayerList(rows) {
     playerList.children().not(":last").remove();
     playerContainer.children(".alert").remove();
     if (rows.length) {
-      console.log(rows);
+      // console.log(rows);
       playerList.prepend(rows);
     }
     else {
@@ -43,7 +55,7 @@ function getPlayers() {
     }
   }
 
-  // Function for handling what to render when there are no authors
+  // Function for handling what to render when there are no players
   function renderEmpty() {
     var alertDiv = $("<div>");
     alertDiv.addClass("alert alert-danger");
